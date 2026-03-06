8064d93422ad46b3a2a0d79cbacad9daee824b32
yjc <yjc@163.com>
2026-03-06 15:51:28 +0800

    添加这个"format": "prettier --write .

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <p>App 组件</p>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+
+</script>
+
+<style scoped lang='less'>
+
+</style>
