91cba563535daf6a1ed4181a3b8f80b94e4ecec4
yjc <yjc@163.com>
2026-03-09 14:46:20 +0800

    feat: 箭头函数的配置项调整

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,31 @@ import path from "path";
 
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // 配置 SVG 雪碧图插件
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      // 指定 symbolId 格式
-      symbolId: "icon-[dir]-[name]",
-    }),
-  ],
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      // 配置 SVG 雪碧图插件
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+        // 指定 symbolId 格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+    ],
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), //相对路径别名配置
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/variable.scss";`,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"), //相对路径别名配置
       },
     },
-  },
-});
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/styles/variable.scss";`,
+        },
+      },
+    },
+  }
+}
+);
