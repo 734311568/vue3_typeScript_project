51248dbc832bcd5a594a4c763918820fe92dfcf8
yjc <yjc@163.com>
2026-03-09 14:31:02 +0800

    feat: 样式的集成

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import globalComponents from "@/components";
+import "@/styles/index.scss";
 
 import "virtual:svg-icons-register";
 
@@ -7,7 +8,7 @@ import { createApp } from "vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
-import "@/styles";
+
 import App from "./App.vue";
 const app = createApp(App);
 app.use(ElementPlus, { locale: zhCn });
