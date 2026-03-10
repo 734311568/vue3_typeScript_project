b42b0f86f57ed6067573e3a4f3951f6053678a66
yjc <yjc@163.com>
2026-03-10 21:26:24 +0800

    fix: 调整字体

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,13 +4,12 @@
     <el-row>
       <el-col :span="12" :xs="0">左边 </el-col>
       <el-col :span="12" :xs="24">
-        <el-form> 
+        <el-form>
           <h1>holle</h1>
-           <h1>欢迎来到吊毛基地</h1>
+          <h2>欢迎来到吊毛基地</h2>
         </el-form>
       </el-col>
-      
-    </el-row> 
+    </el-row>
   </div>
 </template>
 
@@ -21,8 +20,8 @@ import { ref } from "vue";
 <style scoped lang="scss">
 .login_container {
   width: 100%; //自适应100%
-  height: 100vh;  //100%视口 vh的作用是保持高度不变
-  background: url("@/assets/images/background.jpg") no-repeat ;  //设置背景 ,不重复
+  height: 100vh; //100%视口 vh的作用是保持高度不变
+  background: url("@/assets/images/background.jpg") no-repeat; //设置背景 ,不重复
   background-size: cover; //覆盖整个div
 }
 </style>
