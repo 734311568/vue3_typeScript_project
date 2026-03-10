9f7618e7ffeca727f6cb92e24ad3df18516f5191
yjc <yjc@163.com>
2026-03-10 20:15:46 +0800

    feat: 路由的注册

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 
 import App from "./App.vue";
-import router from "./router"; //引入路由   
+import router from "./router"; //引入路由
 const app = createApp(App);
 app.use(ElementPlus, { locale: zhCn });
 //注册项目的全局组件
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,17 +2,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "../views/home/index.vue";
-import Login from "../views/login/index.vue";   
+import Login from "../views/login/index.vue";
 
-import {constRouter }from "./router";
-
+import { constRouter } from "./router";
 
 //创建路由
 createRouter({
-    history: createWebHashHistory(),
-     routes: [constRouter],
-     //滚动行为
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+  history: createWebHashHistory(),
+  routes: [constRouter],
+  //滚动行为
+  scrollBehavior: () => ({ left: 0, top: 0 }),
 });
 
 export default router;
