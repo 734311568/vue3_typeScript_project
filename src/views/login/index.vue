9a879fefbe35f54aa75d40cf38b250c5cdc2170f
yjc <yjc@163.com>
2026-03-10 21:24:32 +0800

    feat:  登录页面的背景样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="login">
-    <h1>登录</h1>
+  <div class="login_container">
+    <!---用element-plus布局 一行两列-->
+    <el-row>
+      <el-col :span="12" :xs="0">左边 </el-col>
+      <el-col :span="12" :xs="24">
+        <el-form> 
+          <h1>holle</h1>
+           <h1>欢迎来到吊毛基地</h1>
+        </el-form>
+      </el-col>
+      
+    </el-row> 
   </div>
 </template>
 
@@ -8,4 +18,11 @@
 import { ref } from "vue";
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.login_container {
+  width: 100%; //自适应100%
+  height: 100vh;  //100%视口 vh的作用是保持高度不变
+  background: url("@/assets/images/background.jpg") no-repeat ;  //设置背景 ,不重复
+  background-size: cover; //覆盖整个div
+}
+</style>
