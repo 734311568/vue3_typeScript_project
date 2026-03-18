187a5095369e7463e346ed1b22dc7cba85e8e4dc
yjc <yjc@163.com>
2026-03-18 13:44:26 +0800

    feat: 国际化

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ import zhCn from "element-plus/es/locale/lang/zh-cn";
 import App from "./App.vue";
 import router from "./router";
 import pinia from "./store";
+
 const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 app.use(ElementPlus);
 //注册项目的全局组件
 app.use(globalComponents);
