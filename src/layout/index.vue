57073d824a30ef92ef984df1eeaa5a89e69f1494
yjc <yjc@163.com>
2026-03-19 14:56:58 +0800

    feat: 菜单栏的静态搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,6 +3,21 @@
     <!-- 左侧菜单 -->
     <div class="layout_sider">
       <Logo></Logo>
+      <!--滚动组件-->
+      <el-scrollbar class="scrollbar">
+        <!---菜单组件-->
+        <el-menu  background-color="#001529" text-color="white">
+          <el-menu-item index="1"  >首页</el-menu-item>
+          <el-menu-item index="2"  >数据大屏幕</el-menu-item>
+          <!--折叠菜单-->
+          <el-sub-menu index="3">
+            <template #title>权限管理</template>
+            <el-menu-item index="2-1">用户管理</el-menu-item>
+            <el-menu-item index="2-2">角色管理</el-menu-item>
+            <el-menu-item index="2-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!--顶部导航-->
     <div class="layout_tabbar">底部导航</div>
@@ -28,7 +43,13 @@ import Logo from "@/layout/logo/index.vue";
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height); //100vh-logo高度
+    }
   }
+
   .layout_tabbar {
     position: fixed;
     width: calc(100% - $base-menu-width);
@@ -37,6 +58,7 @@ import Logo from "@/layout/logo/index.vue";
     top: 0px;
     left: $base-menu-width;
   }
+
   .layout_main {
     position: absolute;
     width: calc(100% - $base-menu-width);
